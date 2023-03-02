6ee04ed9b1ead71c8fe09831ae72d42c9fbc0671
Aaron Humphres <aaronhumphres@yahoo.com>
2023-03-02 12:59:52 -0700

    finished step 2

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 ## User Stories
 - As a User, when I click the View All My Cats link, I want to see a page listing all of my cats.
+- AAU, when I click on a cat in the cat list, I want to see a page that displays all the details for that cat.
 
 
 ## Routes
 
 route controller action 
 /cats  cats  index  ciew all cats
-/cats/:id 
+/cats/:id show cats view one cat
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import Cat
+
 # temporary cats for building templates
-#cats = [
- # {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  #{'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-#]
+# cats = [
+#     {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+#     {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+#     {'name': 'Tubs', 'breed': 'ragdoll', 'description': 'chunky lil guy', 'age': 0},
+# ]
 
 # Create your views here.
 # view functions match urls to code (like controllers in Express)
@@ -16,13 +18,19 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-# index route
+# index route for cats
 def cats_index(request):
     # just like we passed data to our templates in express
     # we pass data to our templates through our view functions
     # we can gather relations from SQL using our model methods
     cats = Cat.objects.all()
-    return render(request, 'cats/index.html', { 'cats': cats})
+    return render(request, 'cats/index.html', { 'cats': cats })
 
+# detail route for cats
+# cat_id is defined, expecting an integer, in our url
+def cats_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+
+    return render(request, 'cats/detail.html', { 'cat': cat })
 
 
